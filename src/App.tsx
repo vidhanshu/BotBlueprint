0f5a31c46ca9c310a0aa23c0d1c8426e1a0e2c2e
vidhanshu borade <84308141+vidhanshu@users.noreply.github.com>
2024-05-16 13:44:59 +0000

    Add dnd for message node

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import ReactFlow, {
   Controls,
   Edge,
   Node,
+  ReactFlowInstance,
+  ReactFlowProvider,
   addEdge,
   useEdgesState,
   useNodesState,
@@ -19,15 +21,25 @@ import TextNode from "./components/react-flow/text-node";
 const initialNodes: Node[] = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  { id: "3", position: { x: 0, y: 100 }, data: { label: "3" }, type:"textUpdater" },
+  {
+    id: "3",
+    position: { x: 0, y: 100 },
+    data: { label: "3" },
+    type: "textNode",
+  },
 ];
 const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
-const nodeTypes = { textUpdater: TextNode };
+const nodeTypes = { textNode: TextNode };
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 ///---------------------------------------------------------------------------------------
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [reactFlowInstance, setReactFlowInstance] =
+    useState<ReactFlowInstance | null>(null);
 
   const onConnect = useCallback(
     (connection: Connection) =>
@@ -35,22 +47,59 @@ export default function App() {
     [setEdges]
   );
 
+  const onDragOver = useCallback((event: any) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event: any) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      // check if the dropped element is valid
+      if (typeof type === "undefined" || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance?.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      })!;
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance]
+  );
+
   return (
     <div style={{ height: "90vh" }}>
       <Header />
-      <div className="flex flex-grow h-[calc(100vh-56px)]">
-        <ReactFlow
-          nodeTypes={nodeTypes}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          nodes={nodes}
-          edges={edges}
-        >
-          <Controls />
-        </ReactFlow>
-        <Sidebar />
-      </div>
+      <ReactFlowProvider>
+        <div className="flex flex-grow h-[calc(100vh-56px)]">
+          <ReactFlow
+            nodeTypes={nodeTypes}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onDragOver={onDragOver}
+            onDrop={onDrop}
+            onInit={setReactFlowInstance}
+            nodes={nodes}
+            edges={edges}
+          >
+            <Controls />
+          </ReactFlow>
+          <Sidebar />
+        </div>
+      </ReactFlowProvider>
     </div>
   );
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,21 @@
 import { MessageSquareText } from "lucide-react";
 
 const Sidebar = () => {
-  const onDragStart = (event, nodeType) => {
+  const onDragStart = (
+    event: React.DragEvent<HTMLButtonElement>,
+    nodeType: string
+  ) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
   };
 
   return (
     <aside className="w-96 h-[calc(100vh-56px)] border-l border-gray-200 p-4">
-      <button className="border rounded-sm border-blue-500 w-full p-4">
+      <button
+        draggable
+        onDragStart={(event) => onDragStart(event, "textNode")}
+        className="border rounded-sm border-blue-500 w-full p-4"
+      >
         <MessageSquareText />
         <h1>Message</h1>
       </button>
